60c5908d0e8ef2fe8db2a15651881b14a814d493
Pedro Palma <pedrolucaspalma@gmail.com>
2023-06-08 18:26:02 -0300

    upd

---
+++ hello-world.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name string = "pedro"
+    fmt.Println("testing go. Hello " + name)
+
+    secondName := "Lucas"
+    fmt.Println("testing go. Hello " + secondName)
+}
